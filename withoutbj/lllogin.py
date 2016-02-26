21e9de0de7b7de046ddebf6949aa5a544c2c9180
Ryan Gooler <ryan.gooler@gmail.com>
2016-02-25 21:58:44 -0800

    Make lllogin object header compatable with SL version

--- withoutbj/lllogin.py
+++ withoutbj/lllogin.py
@@ -45,5 +45,55 @@ class LLLogin():
 	def __init__(self):
 		pass
 
+	def connect(uri, credentials):
+		pass
+
+	def disconnect():
+		pass
+
+	def getEventPump():
+		"""
+		Event API
+
+		LLLogin will issue multiple events to it pump to indicate the 
+		progression of states through login. The most important 
+		states are "offline" and "online" which indicate auth failure 
+		and auth success respectively.
+
+		pump: login (tweaked)
+		These are the events posted to the 'login' 
+		event pump from the login module.
+		{
+			state		:	string, // See below for the list of states.
+			progress	:   real // for progress bar.
+			data		:   LLSD // Dependent upon state.
+		}
+		
+		States for method 'login_to_simulator'
+		offline - set initially state and upon failure. data is the server response.
+		srvrequest - upon uri rewrite request. no data.
+		authenticating - upon auth request. data, 'attempt' number and 'request' llsd.
+		downloading - upon ack from auth server, before completion. no data
+		online - upon auth success. data is server response.
+
+
+		Dependencies:
+		pump: LLAres 
+		LLLogin makes a request for a SRV record from the uri provided by the connect method.
+		The following event pump should exist to service that request.
+		pump name: LLAres
+		request = {
+			op : "rewriteURI"
+			uri : string
+			reply : string
+
+		pump: LLXMLRPCListener
+		The request merely passes the credentials LLSD along, with one additional 
+		member, 'reply', which is the string name of the event pump to reply on. 
+		"""
+		pass
+
+	
+
 if __name__ == "__main__":
     pass
